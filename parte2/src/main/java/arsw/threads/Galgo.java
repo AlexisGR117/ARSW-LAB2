feada53bf3a82cfff575e0f4b4bccc5131b1bdb4
AlexisGR117 <jefer12543@gmail.com>
2023-08-26 17:58:03 -0500

    Segunda Parte

--- parte2/src/main/java/arsw/threads/Galgo.java
+++ parte2/src/main/java/arsw/threads/Galgo.java
@@ -2,51 +2,68 @@ package arsw.threads;
 
 /**
  * Un galgo que puede correr en un carril
- * 
+ *
  * @author rlopez
- * 
  */
 public class Galgo extends Thread {
-	private int paso;
-	private Carril carril;
-	RegistroLlegada regl;
+    private final RegistroLlegada regl;
+    private final Carril carril;
+    private int paso;
+    private boolean dormido;
 
-	public Galgo(Carril carril, String name, RegistroLlegada reg) {
-		super(name);
-		this.carril = carril;
-		paso = 0;
-		this.regl=reg;
-	}
+    public Galgo(Carril carril, String name, RegistroLlegada reg) {
+        super(name);
+        this.carril = carril;
+        paso = 0;
+        this.regl = reg;
+    }
 
-	public void corra() throws InterruptedException {
-		while (paso < carril.size()) {			
-			Thread.sleep(100);
-			carril.setPasoOn(paso++);
-			carril.displayPasos(paso);
-			
-			if (paso == carril.size()) {						
-				carril.finish();
-				int ubicacion=regl.getUltimaPosicionAlcanzada();
-				regl.setUltimaPosicionAlcanzada(ubicacion+1);
-				System.out.println("El galgo "+this.getName()+" llego en la posicion "+ubicacion);
-				if (ubicacion==1){
-					regl.setGanador(this.getName());
-				}
-				
-			}
-		}
-	}
+    public void corra() throws InterruptedException {
+        while (paso < carril.size()) {
+            if (dormido) this.dormir();
+            Thread.sleep(100);
+            carril.setPasoOn(paso++);
+            carril.displayPasos(paso);
+            if (paso == carril.size()) {
+                carril.finish();
+                this.registrarLlegada();
+            }
+        }
+    }
 
+    public void registrarLlegada() {
+        synchronized (regl) {
+            int ubicacion = regl.getUltimaPosicionAlcanzada();
+            regl.setUltimaPosicionAlcanzada(ubicacion + 1);
+            System.out.println("El galgo " + this.getName() + " llego en la posicion " + ubicacion);
+            if (ubicacion == 1) regl.setGanador(this.getName());
+        }
+    }
 
-	@Override
-	public void run() {
-		
-		try {
-			corra();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+    public void dormir() {
+        synchronized (regl) {
+            try {
+                regl.wait();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 
-	}
+    public void cambiarEstadoDormido() {
+        dormido = !dormido;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            corra();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
--- parte2/src/main/java/arsw/threads/MainCanodromo.java
+++ parte2/src/main/java/arsw/threads/MainCanodromo.java
@@ -1,17 +1,14 @@
 package arsw.threads;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-
 public class MainCanodromo {
 
+    private static final RegistroLlegada reg = new RegistroLlegada();
     private static Galgo[] galgos;
-
     private static Canodromo can;
-
-    private static RegistroLlegada reg = new RegistroLlegada();
 
     public static void main(String[] args) {
         can = new Canodromo(17, 100);
@@ -24,23 +21,29 @@ public class MainCanodromo {
 
                     @Override
                     public void actionPerformed(final ActionEvent e) {
-						//como acción, se crea un nuevo hilo que cree los hilos
+                        //como acción, se crea un nuevo hilo que cree los hilos
                         //'galgos', los pone a correr, y luego muestra los resultados.
                         //La acción del botón se realiza en un hilo aparte para evitar
                         //bloquear la interfaz gráfica.
                         ((JButton) e.getSource()).setEnabled(false);
                         new Thread() {
+                            @Override
                             public void run() {
                                 for (int i = 0; i < can.getNumCarriles(); i++) {
                                     //crea los hilos 'galgos'
-                                    galgos[i] = new Galgo(can.getCarril(i), "" + i, reg);
+                                    galgos[i] = new Galgo(can.getCarril(i), String.valueOf(i), reg);
                                     //inicia los hilos
                                     galgos[i].start();
-
                                 }
-                               
-				can.winnerDialog(reg.getGanador(),reg.getUltimaPosicionAlcanzada() - 1); 
-                                System.out.println("El ganador fue:" + reg.getGanador());
+                                for (Galgo galgo : galgos) {
+                                    try {
+                                        galgo.join();
+                                    } catch (InterruptedException ex) {
+                                        Thread.currentThread().interrupt();
+                                    }
+                                }
+                                can.winnerDialog(reg.getGanador(), reg.getUltimaPosicionAlcanzada() - 1);
+                                System.out.println("El ganador fue: " + reg.getGanador());
                             }
                         }.start();
 
@@ -52,6 +55,7 @@ public class MainCanodromo {
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        for (Galgo galgo : galgos) galgo.cambiarEstadoDormido();
                         System.out.println("Carrera pausada!");
                     }
                 }
@@ -61,6 +65,10 @@ public class MainCanodromo {
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        for (Galgo galgo : galgos) galgo.cambiarEstadoDormido();
+                        synchronized (reg) {
+                            reg.notifyAll();
+                        }
                         System.out.println("Carrera reanudada!");
                     }
                 }
